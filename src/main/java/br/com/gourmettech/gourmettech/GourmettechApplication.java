7706f03a1215a78f8c18d76dd5679f5a08b31e57
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2025-02-12 20:55:45 -0300

    feat: create project

---
+++ src/main/java/br/com/gourmettech/gourmettech/GourmettechApplication.java
@@ -0,0 +1,13 @@
+package br.com.gourmettech.gourmettech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmettechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmettechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gourmettech/gourmettech/GourmettechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gourmettech.gourmettech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmettechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
